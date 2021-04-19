7c4cd7fa469f32007a0c4c23c7f7a2df8a3b3f18
Akash Mourya <mouryakash883@gmail.com>
2021-04-20 01:13:57 +0530

    Getting user's location

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi there!!</div>;
-};
+/* const App = () => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+      );
+
+      return <div>Hi there!!</div>;
+    }; */
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Location: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
