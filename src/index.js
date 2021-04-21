93188d08e3866b11b3d2af4f3c251c482fb7992a
Akash Mourya <mouryakash883@gmail.com>
2021-04-22 02:31:40 +0530

    Refactoring data loading

--- src/index.js
+++ src/index.js
@@ -17,16 +17,11 @@ class App extends React.Component {
     //THIS IS THE ONLY TIME we do direct assignment
     //to this.state
     this.state = { lat: null, errorMessage: '' };
-
+  }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //We called setstate
-        //We can't write like : (this.state.lat=position.coords.latitude)
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
   //React says we have to define render!!
