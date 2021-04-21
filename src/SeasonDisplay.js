118eec4067353e50891cb1a4be70cfaef1ee48d4
Akash Mourya <mouryakash883@gmail.com>
2021-04-22 03:01:29 +0530

    Added icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -8,8 +8,16 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>DisplaySeason</div>;
+  const text =
+    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
