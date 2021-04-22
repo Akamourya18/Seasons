781a146970ab9f1748f496f0ab05476a5cd05d6b
Akash Mourya <mouryakash883@gmail.com>
2021-04-23 02:59:17 +0530

    Styling done

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,15 @@
+import './SeasonDisplay.css';
 import React from 'react';
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake',
+  },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -8,14 +19,12 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${icon} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 /* const App = () => {
       window.navigator.geolocation.getCurrentPosition(
@@ -27,7 +28,7 @@ class App extends React.Component {
     if (!this.state.lat && this.state.errorMessage)
       return <div>Error: {this.state.errorMessage}</div>;
 
-    return <div>Loading...</div>;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
