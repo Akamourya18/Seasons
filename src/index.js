77be01cc0a6b44407bacbeee6e778447fb0e91fe
Akash Mourya <mouryakash883@gmail.com>
2021-04-23 03:03:41 +0530

    Final styling done

--- src/index.js
+++ src/index.js
@@ -3,32 +3,30 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-/* const App = () => {
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-      );
-
-      return <div>Hi there!!</div>;
-    }; */
-
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
+
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
-  //React says we have to define render!!
-  render() {
-    if (this.state.lat && !this.state.errorMessage)
-      return <SeasonDisplay lat={this.state.lat} />;
 
-    if (!this.state.lat && this.state.errorMessage)
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
